e626b954b51521168490bb4106a96c2f5ce3eb9a
robertsn808 <robertsn@hawaii.edu>
2025-07-31 23:43:31 -1000

    hope

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,12 +7,9 @@ export default defineConfig({
   css: {
     postcss: './postcss.config.js',
   },
-  build: {
-    cssMinify: 'lightningcss',
-  },
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 10000,
     proxy: {
       '/api': 'http://localhost:7000',
       '/attached_assets': 'http://localhost:7000'
